95d9b129a9d6d65bf825acbc8cebca3807ac8245
Donnyves Laroque <donnyves@gmail.com>
2019-03-27 16:56:06 -0700

    Add project files.

---
+++ SportsStore.Domain/Concrete/EFDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using SportsStore.Domain.Entities;
+using SportsStore.Domain.Abstract;
+
+
+
+
+namespace SportsStore.Domain.Concrete
+{
+    public class EFDbContext : DbContext
+    {
+        public DbSet<Product>Products { get; set; }
+    }
+}
---
+++ SportsStore.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SportsStore.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SportsStore.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8e87d221-568e-45e2-883f-7b400a72c347")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
